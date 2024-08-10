1ff4a0d64aa2e7d9cf8491ea586a9a39aa7cfe65
wondewosen <112129234+wondewosen@users.noreply.github.com>
2024-08-10 16:21:58 +0300

    Update Advanced SQL Project 3.sql

--- Advanced SQL Project 3.sql
+++ Advanced SQL Project 3.sql
@@ -1,3 +1,58 @@
+name: Deploy
+
+on:
+  push:
+    branches:
+      - main
+
+jobs:
+  build:
+    name: Build
+    runs-on: ubuntu-latest
+
+    steps:
+      - name: Checkout repo
+        uses: actions/checkout@v2
+
+      - name: Setup Node
+        uses: actions/setup-node@v1
+        with:
+          node-version: 18
+
+      - name: Install dependencies
+        uses: bahmutov/npm-install@v1
+
+      - name: Build project
+        run: npm run build
+
+      - name: Upload production-ready build files
+        uses: actions/upload-artifact@v2
+        with:
+          name: production-files
+          path: ./dist
+
+  deploy:
+    name: Deploy
+    needs: build
+    runs-on: ubuntu-latest
+    if: github.ref == 'refs/heads/main'
+
+    steps:
+      - name: Download artifact
+        uses: actions/download-artifact@v2
+        with:
+          name: production-files
+          path: ./dist
+
+      - name: Deploy to GitHub Pages
+        uses: peaceiris/actions-gh-pages@v3
+        with:
+          github_token: ${{ secrets.GITHUB_TOKEN }}
+          publish_dir: ./dist
+
+
+
+
 --Projects 3 and 4: SQL and Power BI
 --Use Microsoft's AdventureWorks sample database for these projects
 --Part 1: SQL Related Questions: Due Saturday, April 6
